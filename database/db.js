40a5cc973af8bc0d41bb1beae53e5d757870ccc4
Asisipho <missfente@gmail.com>
2023-09-11 10:55:56 +0200

    n

---
+++ database/db.js
@@ -0,0 +1,14 @@
+import pgPromise from 'pg-promise';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const connectPromise = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+};
+
+const db = pgPromise()(connectPromise);
+
+db.connect();
+//export the database
+export default db;
